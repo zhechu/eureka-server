ae795f64e07fa3d075c4d77703a9c6e985c92aeb
zhechu <ling-yu-wang@qq.com>
2019-07-23 00:10:54 +0800

    升级到 2.x 版本

--- src/main/java/com/wise/eureka/EurekaServer.java
+++ src/main/java/com/wise/eureka/EurekaServer.java
@@ -1,29 +1,29 @@
 package com.wise.eureka;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import lombok.extern.slf4j.Slf4j;
-
-@SpringBootApplication
 @EnableEurekaServer
-@Slf4j
+@SpringBootApplication
 public class EurekaServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServer.class, args);
 	}
 
-    @Value("${server.port}")
-    private String port;
-    
-    @PostConstruct
-    public void finishInit(){
-        log.info("Start {} with port:{}",EurekaServer.class.getSimpleName(),port);
-    }
-    
+	@EnableWebSecurity
+	public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			super.configure(http);
+		}
+
+	}
+
 }
